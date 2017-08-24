a7c3d6c9024be027601f2da3e640a3ffc2fb1b53
Dan Tolbert <dtolbert@bandwidth.com>
2017-08-24 15:23:21 -0400

    remove logic for now

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ function startServer() {
 	debug('Starting Server');
 	app.use(bodyParser.json());
 	app.use(express.static('public'))
-	app.use('/callback/', require('./bin/routes.js'));
-	/// catch 404 and forward to error handler
-	app.use(function (req, res, next) {
-		//debug(req)
-		debug(req.body)
-		debug(req.url)
-		var err = new Error('not found');
-		err.status = 404;
-		res.sendStatus(404, 'Not Found')
-	});
+	// app.use('/callback/', require('./bin/routes.js'));
+	// /// catch 404 and forward to error handler
+	// app.use(function (req, res, next) {
+	// 	//debug(req)
+	// 	debug(req.body)
+	// 	debug(req.url)
+	// 	var err = new Error('not found');
+	// 	err.status = 404;
+	// 	res.sendStatus(404, 'Not Found')
+	// });
 
-	// production error handler, no stacktraces leaked to user
-	app.use(function (err, req, res, next) {
-		// res.status(err.status || 500);
-		debug(err);
-		if (typeof(err.status) === 'undefined') {
-			// res.send({
-			// 	status: 'error',
-			// 	error: 'service error'
-			// });
-		} else {
-			// res.send({
-			// 	status: 'error',
-			// 	error: err.message
-			// });
-		}
-	});
+	// // production error handler, no stacktraces leaked to user
+	// app.use(function (err, req, res, next) {
+	// 	// res.status(err.status || 500);
+	// 	debug(err);
+	// 	if (typeof(err.status) === 'undefined') {
+	// 		// res.send({
+	// 		// 	status: 'error',
+	// 		// 	error: 'service error'
+	// 		// });
+	// 	} else {
+	// 		// res.send({
+	// 		// 	status: 'error',
+	// 		// 	error: err.message
+	// 		// });
+	// 	}
+	// });
 
 	const port = process.env.PORT || 3000;
 	app.listen(port, process.env.HOST || "0.0.0.0", function () {
